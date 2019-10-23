d9058a1484d9f70a962cf72199ae9be10eeafccc
Himadieiev Serhii <serhii.himadieiev@linecorp.com>
2019-10-23 17:48:29 +0900

    fix formatting

--- src/main/java/me/flash/distributedbatch/DistributedBatchApplication.java
+++ src/main/java/me/flash/distributedbatch/DistributedBatchApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DistributedBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DistributedBatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedBatchApplication.class, args);
+    }
 
 }
