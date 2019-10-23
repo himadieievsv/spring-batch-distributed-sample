286044e6fe200f94b8dbe0209228e9abc5c47064
Himadieiev Serhii <serhii.himadieiev@linecorp.com>
2019-10-23 16:31:38 +0900

    Initial commit

---
+++ src/main/java/me/flash/distributedbatch/DistributedBatchApplication.java
@@ -0,0 +1,13 @@
+package me.flash.distributedbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/flash/distributedbatch/DistributedBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package me.flash.distributedbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
