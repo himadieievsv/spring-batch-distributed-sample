1b3678ac1cdfb40f4646cd0d69e4e35b2b36d483
Himadieiev Serhii <serhii.himadieiev@linecorp.com>
2019-10-24 20:19:51 +0900

    create runnable classes

---
+++ producer/src/main/java/me/flash/distributedbatch/producer/ProducerApplication.java
@@ -0,0 +1,13 @@
+package me.flash.distributedbatch.producer;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class ProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+}
