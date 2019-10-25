67b1ce7fcefcb3523f1239451f8b9845f429be05
Himadieiev Serhii <serhii.himadieiev@linecorp.com>
2019-10-25 17:56:26 +0900

    alternative shutdown mechanism

--- producer/src/main/java/me/flash/distributedbatch/producer/config/ShutdownListener.java
+++ producer/src/main/java/me/flash/distributedbatch/producer/config/ShutdownListener.java
@@ -1,15 +1,27 @@
 package me.flash.distributedbatch.producer.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.amqp.dsl.AmqpInboundChannelAdapterSMLCSpec;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Component
 public class ShutdownListener implements JobExecutionListener {
+
+    @Autowired
+    private List<DisposableBean> disposableBeans;
+
+    @Autowired
+    private AmqpInboundChannelAdapterSMLCSpec channelAdapterSMLCSpec;
 
     @Override
     public void beforeJob(JobExecution jobExecution) {
@@ -18,6 +30,16 @@ public class ShutdownListener implements JobExecutionListener {
     @Override
     public void afterJob(JobExecution jobExecution) {
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.schedule(() -> System.exit(0), 10, TimeUnit.SECONDS);
+        scheduler.schedule(() -> {
+            channelAdapterSMLCSpec.stop();
+            for (DisposableBean disposableBean : disposableBeans) {
+                try {
+                    disposableBean.destroy();
+                } catch (Exception e) {
+                    log.warn("Can't destroy {}", disposableBean);
+                }
+            }
+            scheduler.shutdown();
+        }, 10, TimeUnit.SECONDS);
     }
 }
